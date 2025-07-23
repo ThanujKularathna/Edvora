a6efdf86b94ff13d5bc09f683678b84851b1bb76
Thanuja Kularathna <thanujakularathna119@gmail.com>
2025-07-24 01:21:16 +0530

    removed notification controller imports and routes

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const assignmentRoutes = require('./routes/assignmentRoute');
 const quizRoutes = require('./routes/quizRoutes');
-const notificationRoutes = require('./routes/notificationRoutes');
 
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -33,12 +32,6 @@ app.use(cookieParser());
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/assignments', assignmentRoutes);
 app.use('/api/v1/quizzes', quizRoutes);
-app.use('/api/v1/notifications', notificationRoutes);
-
-// Serve notifications test page
-app.get('/notifications', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notifications.html'));
-});
 
 app.all('*', (req, res, next) => {
   res.status(404).json({
--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const notificationController = require('../controllers/notificationController');
+// const express = require('express');
+// const notificationController = require('../controllers/notificationController');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/send', notificationController.sendNotification);
-router.post('/send-to-class', notificationController.sendNotification);
-router.post('/send-to-role', notificationController.sendNotification);
+// router.post('/send', notificationController.sendNotification);
+// router.post('/send-to-class', notificationController.sendNotification);
+// router.post('/send-to-role', notificationController.sendNotification);
 
-module.exports = router;
+// module.exports = router;
